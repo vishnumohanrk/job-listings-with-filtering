b37c09a744690cefab684651b3c614bd9d4aad07
vishnumohanrk <mohanvish1999@gmail.com>
2023-03-20 12:34:39 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function IndexPage() {
+  return (
+    <article className="mx-auto max-w-screen-md p-4">
+      <h2 className="text-4xl font-bold">Lorem, ipsum.</h2>
+      <p className="my-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat iste,
+        temporibus magni, distinctio esse sequi debitis, quos ex non accusamus
+        ab error veritatis nostrum eius. Quas, totam fugit? Vitae, nam.
+      </p>
+    </article>
+  );
+}
